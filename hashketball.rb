8e5aebc3768363c2b8f32af60d0291edbc457de8
John Pena <jpena91@gmail.com>
2019-03-09 18:57:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -245,6 +245,6 @@ def winning_team
   winer = ""
   all_players = game_hash[:home][:players].merge(game_hash[:away][:players])
   game_hash.each do |location, stats|
-    stats.select {|key,values| keys.has_key?(:)
+    stats.select {|key,values| keys.has_key?(:players)}
 end 
 
